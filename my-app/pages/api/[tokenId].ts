1158e7d6e32a9355b18b01dd3b4426345d4269f4
yanukadeneth99 <yanukadeneth99@gmail.com>
2022-07-13 20:25:06 +0530

    Fixed Base URI

--- my-app/pages/api/[tokenId].ts
+++ my-app/pages/api/[tokenId].ts
@@ -2,9 +2,9 @@ export default function handler(req: any, res: any) {
   const tokenId = req.query.tokenId;
   const name = `Meme NFT #${tokenId}`;
   const description = "Meme NFTs";
-  const image = `#/${tokenId}`;
+  const image = `https://raw.githubusercontent.com/yanukadeneth99/Meme-NFT/master/my-app/public/nft/${tokenId}.png`;
 
-  return res.status(200).res.json({
+  return res.status(200).json({
     name: name,
     description: description,
     image: image,
